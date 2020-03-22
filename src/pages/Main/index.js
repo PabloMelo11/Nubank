99a8d2cfa75936cf8e03008abe4dfa46977cfe4f
Pablo Melo <pablomelodev@gmail.com>
2020-03-22 16:35:26 -0300

    Menu

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,47 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
-import {Container} from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Menu />
+
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponivel</Title>
+            <Description>R$ 197.611,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferencia de R$ 20,00 recebida de Debora hoje as 19:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
